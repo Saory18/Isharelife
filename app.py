b1107ad471c5a470a1a7c74306584148fd5fc993
Saory Nayara Vieira Nakabori <97962041+Saory18@users.noreply.github.com>
2024-10-14 00:20:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -224,9 +224,9 @@ elif st.session_state[PAGE_SELECTION_KEY] == "Chat":
         "claude-v3-sonnet": "anthropic.claude-3-sonnet-20240229-v1:0",
         "claude-v3-haiku": "anthropic.claude-3-haiku-20240307-v1:0",
         "claude-v3-5-sonnet":"anthropic.claude-3-5-sonnet-20240620-v1:0",
-        "claude-v2.1": "anthropic.claude-v2:1",
-        "claude-v2": "anthropic.claude-v2",
-        "claude-instant": "anthropic.claude-instant-v1",
+        # "claude-v2.1": "anthropic.claude-v2:1",
+        # "claude-v2": "anthropic.claude-v2",
+        # "claude-instant": "anthropic.claude-instant-v1",
     }
 
     model_id_options = list(models.keys())
